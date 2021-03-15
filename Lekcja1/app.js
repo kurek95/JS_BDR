93a4633712d8a199548331e139eb50ed63c60ecd
Damian Kuras <damian@kuras.com.pl>
2021-03-15 13:20:02 +0100

    repozytorium biletu do rozwoju

---
+++ Lekcja1/app.js
@@ -0,0 +1,46 @@
+/* Zadanie domowe - napisać skrypt w js,
+który wyświetli 10 skrótów klawiszowych
+w visual studio code w konsoli
+*/
+// Przykład1 console.log('Ctrl + C', 'Kopiowanie');
+
+// console.time('test'); //sprawdzenie ile czasu wykonują się operacje
+// console.log('ctr + c', 'kopiowanie');
+// console.timeEnd('test');
+
+// if (confirm('Czy jesteś człowiekiem?')) {
+//     alert('W takim razie witaj na stronie!');
+// }
+// else {
+//     alert('Stop spamming, Mr. bot');
+// }
+
+// console.log(confirm('Czy jest ok?')); //w odpowiedzi true albo false
+
+// console.log(prompt('Podaj swoje imię'));
+// alert('Witaj ' + prompt('Podaj swoje imię'));
+
+// console.log('Hello', 'World', window);
+// console.warn('Hello'); // warning wyswietalny na zolto
+// console.error('Hello world', console); //wyswietelenie erroru
+
+// console.log(document.getElementsByTagName('b')[0]); //wyswietlenie pierwszego tagu b w tablicy
+// console.dir(document.getElementsByTagName('b')[0]);
+
+// console.groupCollapsed('Grupa1'); //grupowanie
+// console.log('aaaaa');
+// console.log('aaaaa');
+// console.log('aaaaa');
+// console.log('aaaaa');
+// console.log('aaaaa');
+// console.log('aaaaa');
+// console.log('aaaaa');
+// console.groupEnd();
+
+// document.write('Hello\n<br>');
+// document.write('Hello\r\n');
+// document.writeln('Hello World');
+// document.writeln('Hello World');
+
+// document.body.appendChild(document.createElement('dziecko')); //dodanie dziecka do kodu
+// document.body.innerHTML = '<b>Dzień dobry</b>'; //dodanie zawartości, która zastępuje pozostałą zawartość
